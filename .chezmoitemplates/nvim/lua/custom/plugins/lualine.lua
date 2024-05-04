88ddda7c259b4bac7d341f6502d42c3566cee5ae
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2024-05-03 22:49:46 -0400

    apply lua formatting

--- .chezmoitemplates/nvim/lua/custom/plugins/lualine.lua
+++ .chezmoitemplates/nvim/lua/custom/plugins/lualine.lua
@@ -1,22 +1,22 @@
 return {
-	{
-		"nvim-lualine/lualine.nvim",
-		event = "VeryLazy",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		opts = {
-			options = {
-				theme = "catppuccin",
-				extensions = {
-					"lazy",
-					"mason",
-					"neo-tree",
-					"nvim-dap-ui",
-					"oil",
-					"quickfix",
-					"trouble",
-				},
-			},
-		},
-	},
+  {
+    'nvim-lualine/lualine.nvim',
+    event = 'VeryLazy',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {
+      options = {
+        theme = 'catppuccin',
+        extensions = {
+          'lazy',
+          'mason',
+          'neo-tree',
+          'nvim-dap-ui',
+          'oil',
+          'quickfix',
+          'trouble',
+        },
+      },
+    },
+  },
 }
 -- vim: ts=2 sts=2 sw=2 et
--- .chezmoitemplates/nvim/lua/custom/plugins/vim-be-good.lua
+++ .chezmoitemplates/nvim/lua/custom/plugins/vim-be-good.lua
@@ -1,7 +1,7 @@
 return {
-	{
-		"ThePrimeagen/vim-be-good",
-		lazy = false,
-	},
+  {
+    'ThePrimeagen/vim-be-good',
+    lazy = false,
+  },
 }
 -- vim: ts=2 sts=2 sw=2 et
