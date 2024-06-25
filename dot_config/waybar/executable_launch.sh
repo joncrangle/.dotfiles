5181c8876259650fe55495403fa8a9d76f818a58
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2024-06-25 15:14:32 -0400

    waybar test

---
+++ dot_config/waybar/executable_launch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# ----------------------------------------------------- 
+# Quit all running waybar instances
+# ----------------------------------------------------- 
+killall waybar
+pkill waybar
+sleep 0.2
+
+waybar &
