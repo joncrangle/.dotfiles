a2690e8faa8a1e55b1bb47e6a8099e28303bea15
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2024-09-01 23:27:21 -0400

    configure rust-analyzer

--- .chezmoitemplates/nvim/lua/kickstart/plugins/lspconfig.lua
+++ .chezmoitemplates/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -408,9 +408,6 @@ return {
       tools = { float_win_config = { border = "rounded" } },
       server = {
         on_attach = function(_, bufnr)
-          vim.keymap.set("n", "<leader>cR", function()
-            vim.cmd.RustLsp("codeAction")
-          end, { desc = "Code Action", buffer = bufnr })
           vim.keymap.set("n", "<leader>dr", function()
             vim.cmd.RustLsp("debuggables")
           end, { desc = "Rust Debuggables", buffer = bufnr })
