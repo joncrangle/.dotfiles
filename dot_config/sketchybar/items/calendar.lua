91d32143a2193c30de2af73b1c076ce3850c2a8d
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2024-09-03 01:02:50 -0400

    fix: sketchybar calendar update

--- dot_config/sketchybar/items/calendar.lua
+++ dot_config/sketchybar/items/calendar.lua
@@ -33,64 +33,73 @@ local cal = sbar.add('item', 'calendar', {
   update_freq = 15,
 })
 
-local stdout = io.popen('cal', 'r')
-if not stdout then
-  return ''
-end
-local month = stdout:read('*all')
-stdout:close()
-
-local lines = {}
-local i = 1
-for line in month:gmatch('[^\n]+') do
-  if line:match("%S") then
-    table.insert(lines, line)
+local function update_calendar_items()
+  local stdout = io.popen('cal', 'r')
+  if not stdout then
+    return ''
   end
-  i = i + 1
-end
+  local month = stdout:read('*all')
+  stdout:close()
 
-local function format_line(line)
-  local current_day = tonumber(os.date('%e')) or 0
-  -- Highlight the current day by adding `|` around it
-  return line:gsub('(%s)(%d+)(%s)', function(before, day, after)
-    local num = tonumber(day)
-    if num == current_day then
-      -- Remove spaces around the current day number
-      return before:match('%s') and '|' .. day .. '|' or '|' .. day .. '|' .. after
-    else
-      return before .. day .. after
+  local lines = {}
+  for line in month:gmatch('[^\n]+') do
+    if line:match("%S") then
+      table.insert(lines, line)
     end
-  end)
-end
+  end
 
-for idx, line in ipairs(lines) do
-  local item_name = 'cal_month_line_' .. idx
-  sbar.add('item', item_name, {
-    position = 'popup.' .. cal.name,
-    icon = { drawing = false },
-    width = 180,
-    label = {
-      string = format_line(line),
-      color = idx == 1 and colors.peach or
-          idx == 2 and colors.flamingo or
-          colors.maroon,
-      font = {
-        family = settings.font.text,
-        style = settings.font.style_map['Bold'],
-        size = idx == 1 and 16.0 or 14.0
+  local function format_line(line)
+    local current_day = tonumber(os.date('%e')) or 0
+    -- Highlight the current day by adding `|` around it
+    return line:gsub('(%s)(%d+)(%s)', function(before, day, after)
+      local num = tonumber(day)
+      if num == current_day then
+        -- Remove spaces around the current day number
+        return before:match('%s') and '|' .. day .. '|' or '|' .. day .. '|' .. after
+      else
+        return before .. day .. after
+      end
+    end)
+  end
+
+  for idx, line in ipairs(lines) do
+    local item_name = 'cal_month_line_' .. idx
+    sbar.add('item', item_name, {
+      position = 'popup.' .. cal.name,
+      icon = { drawing = false },
+      width = 180,
+      label = {
+        string = format_line(line),
+        color = idx == 1 and colors.peach or
+            idx == 2 and colors.flamingo or
+            colors.maroon,
+        font = {
+          family = settings.font.text,
+          style = settings.font.style_map['Bold'],
+          size = idx == 1 and 16.0 or 14.0
+        },
       },
-    },
-  })
+    })
+  end
 end
 
-local function update()
+update_calendar_items()
+
+local function update_time()
   local date = os.date('%a. %d %b.')
   local time = tostring(os.date('%I:%M %p')):gsub('^0', '')
   cal:set({ icon = date, label = time })
 end
 
-cal:subscribe('routine', update)
-cal:subscribe('forced', update)
+cal:subscribe('routine', function()
+  update_time()
+  update_calendar_items()
+end)
+
+cal:subscribe('forced', function()
+  update_time()
+  update_calendar_items()
+end)
 
 cal:subscribe('mouse.entered', function()
   cal:set({ background = { border_width = 1 } })
