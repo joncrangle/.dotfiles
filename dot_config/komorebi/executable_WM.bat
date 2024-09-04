57b3a8456cd59f00ec08e20d843c4737a832bfb0
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2024-09-04 11:30:34 -0400

    komorebi startup script tweak

--- dot_config/komorebi/executable_WM.bat
+++ dot_config/komorebi/executable_WM.bat
@@ -1,6 +1,6 @@
 @echo off
 @REM Run komorebic-no-console.exe with the specified configuration and log errors
-pwsh -WindowStyle Hidden -Command Start-Process "$env:USERPROFILE\scoop\apps\komorebi\current\komorebic-no-console.exe" -ArgumentList "start", "--config", "$env:KOMOREBI_CONFIG_HOME\komorebi.json", "--whkd"
+pwsh -WindowStyle Hidden -Command Start-Process "$env:USERPROFILE\scoop\apps\komorebi\current\komorebic-no-console.exe" -ArgumentList "start", "--config", "$env:KOMOREBI_CONFIG_HOME\komorebi.json", "--whkd" -Wait
 
 @REM Start hidden PowerShell script, which runs `zebar open bar --args ...` for every monitor
 powershell -WindowStyle hidden -Command ^
