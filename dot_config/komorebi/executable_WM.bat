f1c120c37c82f1e925dc0a90ed319d481a255186
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2024-10-03 10:50:41 -0400

    update startup script

--- dot_config/komorebi/executable_WM.bat
+++ dot_config/komorebi/executable_WM.bat
@@ -3,9 +3,8 @@
 pwsh -WindowStyle Hidden -Command Start-Process "$env:USERPROFILE\scoop\apps\komorebi\current\komorebic-no-console.exe" -ArgumentList "start", "--config", "$env:KOMOREBI_CONFIG_HOME\komorebi.json", "--whkd"
 
 @REM Wait until komorebi is running before launching zebar
-powershell -WindowStyle hidden -Command ^
+pwsh -WindowStyle hidden -Command ^
   while (-not (Get-Process -Name "komorebi" -ErrorAction SilentlyContinue)) { Start-Sleep -Seconds 5 }; ^
-  @REM Start hidden PowerShell script, which runs `zebar open bar --args ...` for every monitor
-  powershell -WindowStyle hidden -Command ^
-  $monitors = zebar monitors; ^
-  foreach ($monitor in $monitors) { Start-Process -WindowStyle Hidden -FilePath \"zebar\" -ArgumentList \"open bar --args $monitor\" };
+
+@REM Start hidden PowerShell script, which runs `zebar`
+pwsh -WindowStyle Hidden -Command Start-Process "zebar.exe"
