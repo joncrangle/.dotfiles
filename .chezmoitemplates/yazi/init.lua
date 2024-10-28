8b45dd27dbf136383cf96ccecc11c9cb75dd71e6
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2024-10-28 11:20:15 -0400

    yazi relative motions bug fix

--- .chezmoitemplates/yazi/init.lua
+++ .chezmoitemplates/yazi/init.lua
@@ -1,3 +1,5 @@
-require("relative-motions"):setup({ show_numbers = "relative" })
+-- FIX: relative line numbers disabled due to bug
+-- require("relative-motions"):setup({ show_numbers = "relative" })
+require("relative-motions"):setup()
 require("starship"):setup()
 require("zoxide"):setup({ update_db = true })
