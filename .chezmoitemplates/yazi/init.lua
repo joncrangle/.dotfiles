574da0fbc0de45c183501e34dce6b371f09cc740
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2024-11-01 15:06:42 -0400

    remove temp fix for relative-motions plugin

--- .chezmoitemplates/yazi/init.lua
+++ .chezmoitemplates/yazi/init.lua
@@ -1,5 +1,3 @@
--- FIX: relative line numbers disabled due to bug
--- require("relative-motions"):setup({ show_numbers = "relative" })
-require("relative-motions"):setup()
+require("relative-motions"):setup({ show_numbers = "relative" })
 require("starship"):setup()
 require("zoxide"):setup({ update_db = true })
