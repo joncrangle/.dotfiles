d111943d5a2b7d3787934347911dd29ad98aebad
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2024-12-02 23:33:43 -0500

    update fzf opts

--- setup_win.ps1
+++ setup_win.ps1
@@ -188,7 +188,9 @@ Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory
 `$ENV:FZF_DEFAULT_OPTS= " `
 --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 `
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc `
---color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+--color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 `
+--color=selected-bg:#45475a `
+--multi"
 
 # Set FZF_ALT_C_OPTS
 `$env:FZF_ALT_C_OPTS = "--preview 'tree -C {} | Select-Object -First 10'"
