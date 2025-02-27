dfe69c1e94dbeb8a70151ebae6085ea200dd42bd
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2025-02-27 15:31:27 -0500

    don't convert lsp info to md

--- .chezmoitemplates/nvim/lua/plugins/ui.lua
+++ .chezmoitemplates/nvim/lua/plugins/ui.lua
@@ -92,8 +92,6 @@ return {
     opts = {
       lsp = {
         override = {
-          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-          ['vim.lsp.util.stylize_markdown'] = true,
           ['cmp.entry.get_documentation'] = true,
         },
         signature = { auto_open = { enabled = false } },
