d9dd9b80d075dcfff1dc7f6b6aebe8f86e29fc04
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2025-02-27 18:52:13 -0500

    add edgy.nvim

--- .chezmoitemplates/nvim/lua/plugins/ui.lua
+++ .chezmoitemplates/nvim/lua/plugins/ui.lua
@@ -241,5 +241,95 @@ return {
       }
     end,
   },
+  ---@module 'edgy'
+  {
+    'folke/edgy.nvim',
+    event = 'VeryLazy',
+    opts = function()
+      ---@type Edgy.Config|{}
+      local opts = {
+        bottom = {
+          {
+            ft = 'noice',
+            size = { height = 0.3 },
+            filter = function(_, win)
+              return vim.api.nvim_win_get_config(win).relative == ''
+            end,
+          },
+          'Trouble',
+          { ft = 'qf', title = 'QuickFix' },
+          {
+            ft = 'help',
+            size = { height = 0.3 },
+            -- don't open help files in edgy that we're editing
+            filter = function(buf)
+              return vim.bo[buf].buftype == 'help'
+            end,
+          },
+          { title = 'Neotest Output', ft = 'neotest-output-panel', size = { height = 15 } },
+        },
+        left = {
+          { title = 'DBUI', ft = 'dbui' },
+        },
+        right = {
+          { title = 'Avante (Input)', ft = 'AvanteInput', size = { height = 9 } },
+          { title = 'Avante (Selected Files)', ft = 'AvanteSelectedFiles', size = { height = 4 } },
+          { title = 'Avante', ft = 'Avante', size = { width = 0.4 } },
+          { title = 'Grug Far', ft = 'grug-far', size = { width = 0.4 } },
+          { title = 'Neotest Summary', ft = 'neotest-summary' },
+        },
+        keys = {
+          -- increase width
+          ['<c-Right>'] = function(win)
+            win:resize('width', 2)
+          end,
+          -- decrease width
+          ['<c-Left>'] = function(win)
+            win:resize('width', -2)
+          end,
+          -- increase height
+          ['<c-Up>'] = function(win)
+            win:resize('height', 2)
+          end,
+          -- decrease height
+          ['<c-Down>'] = function(win)
+            win:resize('height', -2)
+          end,
+        },
+      }
+
+      -- trouble
+      for _, pos in ipairs { 'top', 'bottom', 'left', 'right' } do
+        opts[pos] = opts[pos] or {}
+        table.insert(opts[pos], {
+          ft = 'trouble',
+          filter = function(_, win)
+            return vim.w[win].trouble
+              and vim.w[win].trouble.position == pos
+              and vim.w[win].trouble.type == 'split'
+              and vim.w[win].trouble.relative == 'editor'
+              and not vim.w[win].trouble_preview
+          end,
+        })
+      end
+
+      -- snacks terminal
+      for _, pos in ipairs { 'top', 'bottom', 'left', 'right' } do
+        opts[pos] = opts[pos] or {}
+        table.insert(opts[pos], {
+          ft = 'snacks_terminal',
+          size = { height = 0.4 },
+          title = '%{b:snacks_terminal.id}: %{b:term_title}',
+          filter = function(_, win)
+            return vim.w[win].snacks_win
+              and vim.w[win].snacks_win.position == pos
+              and vim.w[win].snacks_win.relative == 'editor'
+              and not vim.w[win].trouble_preview
+          end,
+        })
+      end
+      return opts
+    end,
+  },
 }
 -- vim: ts=2 sts=2 sw=2 et
