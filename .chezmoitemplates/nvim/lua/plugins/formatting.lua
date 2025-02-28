83348104f55802c4dedde2f5f84a1d1d7e99f345
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2025-02-28 15:51:50 -0500

    update markdown formatters

--- .chezmoitemplates/nvim/lua/plugins/formatting.lua
+++ .chezmoitemplates/nvim/lua/plugins/formatting.lua
@@ -34,7 +34,8 @@ return {
         javascriptreact = { 'biome', 'prettierd', 'prettier', stop_after_first = true },
         json = { 'biome', 'prettierd', 'prettier', stop_after_first = true },
         lua = { 'stylua' },
-        markdown = { 'markdownlint' },
+        markdown = { 'markdownlint-cli2', 'markdown-toc' },
+        ['markdown.mdx'] = { 'prettier', 'markdownlint-cli2', 'markdown-toc' },
         mysql = { 'sqlfluff' },
         plsql = { 'sqlfluff' },
         python = { 'ruff_format' },
@@ -49,6 +50,7 @@ return {
         zsh = { 'shfmt' },
       },
       formatters = {
+        injected = { options = { ignore_errors = true } },
         kulala = {
           command = 'kulala-fmt',
           args = { 'format', '$FILENAME' },
@@ -56,6 +58,25 @@ return {
         },
         sqlfluff = {
           args = { 'format', '--dialect=ansi', '-' },
+        },
+        formatters = {
+          ['markdown-toc'] = {
+            condition = function(_, ctx)
+              for _, line in ipairs(vim.api.nvim_buf_get_lines(ctx.buf, 0, -1, false)) do
+                if line:find '<!%-%- toc %-%->' then
+                  return true
+                end
+              end
+            end,
+          },
+          ['markdownlint-cli2'] = {
+            condition = function(_, ctx)
+              local diag = vim.tbl_filter(function(d)
+                return d.source == 'markdownlint'
+              end, vim.diagnostic.get(ctx.buf))
+              return #diag > 0
+            end,
+          },
         },
       },
       format_on_save = function(bufnr)
