fd17b3bacf51abb14fe944e344b9ee0923ee1903
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2025-04-01 11:56:22 -0400

    use lsp foldexpr if client supports it

--- .chezmoitemplates/nvim/lua/config/options.lua
+++ .chezmoitemplates/nvim/lua/config/options.lua
@@ -79,9 +79,20 @@ vim.opt.fillchars = {
   eob = ' ',
 }
 vim.opt.foldlevel = 99
-vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
-vim.opt.foldmethod = 'expr'
 vim.opt.foldtext = ''
+vim.opt.foldmethod = 'expr'
+-- Default to treesitter folding
+vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+-- Prefer LSP folding if client supports it
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client and client:supports_method 'textDocument/foldingRange' then
+      local win = vim.api.nvim_get_current_win()
+      vim.wo[win][0].foldexpr = 'v:lua.vim.lsp.foldexpr()'
+    end
+  end,
+})
 
 -- Preview substitutions live while typing
 vim.opt.inccommand = 'split'
