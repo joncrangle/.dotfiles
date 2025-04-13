9a402a3f5eeb367c9d6e17e73e583c18a70d8bcc
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2025-04-13 14:53:24 -0400

    add itchy.nvim

--- .chezmoitemplates/nvim/lua/plugins/custom.lua
+++ .chezmoitemplates/nvim/lua/plugins/custom.lua
@@ -6,5 +6,18 @@ return {
     event = { 'BufReadPre', 'BufNewFile' },
     opts = {},
   },
+  {
+    'joncrangle/itchy.nvim',
+    -- 'itchy.nvim',
+    -- dev = true,
+    event = { 'BufReadPre', 'BufNewFile' },
+    ---@type itchy.Opts
+    opts = {
+      -- debug_mode = true,
+    },
+    keys = {
+      { '<leader>td', mode = { 'n', 'v' }, '<cmd>Itchy run<cr>', desc = '[T]est [D]ebug' },
+    },
+  },
 }
 -- vim: ts=2 sts=2 sw=2 et
