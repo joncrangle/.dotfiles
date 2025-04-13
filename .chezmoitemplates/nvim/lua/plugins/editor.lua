9ab9a21111838db6a48a38af151ad99cc10ca030
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2025-04-13 15:56:48 -0400

    update GrugFarWithin cmd

--- .chezmoitemplates/nvim/lua/plugins/editor.lua
+++ .chezmoitemplates/nvim/lua/plugins/editor.lua
@@ -152,7 +152,7 @@ return {
   {
     'MagicDuck/grug-far.nvim',
     opts = { headerMaxWidth = 80 },
-    cmd = 'GrugFar',
+    cmd = { 'GrugFar', 'GrugFarWithin' },
     keys = {
       {
         '<leader>sr',
