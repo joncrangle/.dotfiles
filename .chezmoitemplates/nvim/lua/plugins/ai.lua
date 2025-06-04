073f8af10cd37c23065f1074cfb3f095cc6d09aa
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2025-06-03 21:26:33 -0400

    updated avante config

--- .chezmoitemplates/nvim/lua/plugins/ai.lua
+++ .chezmoitemplates/nvim/lua/plugins/ai.lua
@@ -10,16 +10,17 @@ return {
       vim.env.SEARXNG_API_URL = string.gsub(searxng_url, ':.*$', ':8090')
     end,
     opts = function()
+      --- Returns the number times 1024
       ---@param num number
       ---@return number
       local function tokens(num)
         return num * 1024
       end
 
-      --- Generate an Avante vendor configuration
+      --- Generate an Avante provider
       ---@param opts { model: string, name: string, temperature?: number, max_tokens?: number, port?: number, min_p?: number, top_p?: number, top_k?: number }
       ---@return AvanteProvider
-      local function generate_vendor(opts)
+      local function generate_provider(opts)
         local port = opts.port or 5001
         return {
           __inherited_from = 'openai',
@@ -27,83 +28,44 @@ return {
           endpoint = string.format('{{- if eq .chezmoi.os "darwin" -}}127.0.0.1{{- else -}}{{- .MacAddress -}}{{- end -}}:%d/v1', port),
           model = opts.model,
           display_name = opts.name,
-          temperature = opts.temperature or 0,
-          max_tokens = opts.max_tokens or tokens(32),
-          min_p = opts.min_p or 0,
-          top_p = opts.top_p or 0.95,
-          top_k = opts.top_k or -1,
+          extra_request_body = {
+            temperature = opts.temperature or 0,
+            max_tokens = opts.max_tokens or tokens(8),
+            min_p = opts.min_p or 0,
+            top_p = opts.top_p or 0.95,
+            top_k = opts.top_k or -1,
+          },
         }
       end
 
       ---@type avante.Config|{}
       return {
         provider = 'copilot',
-        aihubmix = { hide_in_model_selector = true },
-        ['aihubmix-claude'] = { hide_in_model_selector = true },
-        bedrock = { hide_in_model_selector = true },
-        ['bedrock-claude-3.7-sonnet'] = { hide_in_model_selector = true },
-        claude = { hide_in_model_selector = true },
-        ['claude-haiku'] = { hide_in_model_selector = true },
-        ['claude-opus'] = { hide_in_model_selector = true },
-        cohere = { hide_in_model_selector = true },
-        copilot = {
-          model = 'gpt-4.1',
-          temperature = 0.2,
-          max_tokens = tokens(256),
-          display_name = 'OpenAI GPT 4.1',
-        },
-        deepseek = { hide_in_model_selector = true },
-        gemini = {
-          model = 'gemini-2.5-flash-preview-05-20',
-          temperature = 0,
-          max_tokens = tokens(256),
-          display_name = 'Gemini 2.5 Flash Preview',
-        },
-        openai = { hide_in_model_selector = true },
-        ['openai-gpt-4o-mini'] = { hide_in_model_selector = true },
-        vertex = { hide_in_model_selector = true },
-        vertex_claude = { hide_in_model_selector = true },
-        vendors = {
-          ['deepseek-r1'] = generate_vendor { model = 'deepseek-r1-0528-qwen3-8b-mlx', name = 'DeepSeek R1 0528 Qwen3 Distill', temperature = 0.6 },
-          ['qwen3-8b'] = generate_vendor { model = 'qwen3-8b-mlx', name = 'Qwen 3 8B', temperature = 0.6 },
-          ['qwen3-4b'] = generate_vendor { model = 'qwen3-4b-mlx', name = 'Qwen 3 4B', temperature = 0.6 },
-          ['gemma-3-12b-it-qat'] = generate_vendor { model = 'gemma-3-12b-it-qat', name = 'Gemma 3 12B', temperature = 1 },
-          ['gemma-3-4b-it-qat'] = generate_vendor { model = 'gemma-3-4b-it-qat', name = 'Gemma 3 4B', temperature = 1 },
-          ['claude-3.7-sonnet'] = {
-            __inherited_from = 'copilot',
-            model = 'claude-3.7-sonnet',
-            temperature = 0.2,
-            max_tokens = tokens(64),
-            display_name = 'Claude 3.7 Sonnet',
+        providers = {
+          copilot = {
+            model = 'gpt-4.1',
+            extra_request_body = {
+              temperature = 0.2,
+              max_tokens = tokens(256),
+              reasoning_effort = 'high',
+            },
           },
-          ['claude-3.7-sonnet-thought'] = {
-            __inherited_from = 'copilot',
-            model = 'claude-3.7-sonnet-thought',
-            temperature = 0.7,
-            max_tokens = tokens(64),
-            display_name = 'Claude 3.7 Sonnet Thought',
+          gemini = {
+            model = 'gemini-2.5-flash-preview-04-17',
+            extra_request_body = {
+              temperature = 0.2,
+              max_tokens = tokens(256),
+            },
           },
-          ['claude-4-sonnet'] = {
-            __inherited_from = 'copilot',
-            model = 'claude-sonnet-4',
-            temperature = 0.2,
-            max_tokens = tokens(64),
-            display_name = 'Claude 4 Sonnet',
-          },
-          ['o4-mini'] = {
-            __inherited_from = 'copilot',
-            model = 'o4-mini',
-            temperature = 0,
-            max_tokens = tokens(256),
-            display_name = 'OpenAI O4 Mini',
-          },
-          ['gemini-2.5-pro-preview'] = {
-            __inherited_from = 'gemini',
-            model = 'gemini-2.5-pro-preview-05-06',
-            temperature = 0.7,
-            max_tokens = tokens(256),
-            display_name = 'Gemini 2.5 Pro Preview',
-          },
+          vertex = { hide_in_model_selector = true },
+          vertex_claude = { hide_in_model_selector = true },
+          ['qwen3-8b'] = generate_provider { model = 'qwen3-8b-mlx', name = 'Qwen 3 8B', temperature = 0.6 },
+          ['qwen3-4b'] = generate_provider { model = 'qwen3-4b-mlx', name = 'Qwen 3 4B', temperature = 0.6 },
+          ['gemma-3-12b-it-qat'] = generate_provider { model = 'gemma-3-12b-it-qat', name = 'Gemma 3 12B', temperature = 1 },
+          ['gemma-3-4b-it-qat'] = generate_provider { model = 'gemma-3-4b-it-qat', name = 'Gemma 3 4B', temperature = 1 },
+          ['omni-qwen3-4b'] = generate_provider { model = 'mlx-community/Qwen3-4B-4bit-DWQ-053125', temperature = 0.6, port = 10240 },
+          ['omni-qwen3-8b'] = generate_provider { model = 'mlx-community/Qwen3-8B-4bit-DWQ-053125', temperature = 0.6, port = 10240 },
+          ['omni-gemma3-4b'] = generate_provider { model = 'mlx-community/gemma-3-4b-it-4bit-DWQ', temperature = 1, port = 10240 },
         },
         selector = { provider = 'snacks' },
         web_search_engine = { provider = 'searxng' },
