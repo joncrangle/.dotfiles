1c9ab1f209dc08cdb41ed4d07b5824e79d2d2f4f
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2025-06-12 17:29:33 -0400

    nvim indent plugin

--- .chezmoitemplates/nvim/lua/config/options.lua
+++ .chezmoitemplates/nvim/lua/config/options.lua
@@ -106,7 +106,7 @@ vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 vim.o.shiftround = true
 vim.o.expandtab = true
-vim.o.smartindent = true
+vim.o.breakindent = true
 
 -- Don't highlight search results, but highlight incremental search
 --  See `:help hlsearch`
