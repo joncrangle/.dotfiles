9c5412495903a403603414302e80a8a7db9a8af7
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2025-08-29 14:50:40 -0400

    chore: update repo for mini.nvim

--- .chezmoitemplates/nvim/lua/plugins/mini.lua
+++ .chezmoitemplates/nvim/lua/plugins/mini.lua
@@ -1,6 +1,6 @@
 return {
   {
-    'echasnovski/mini.nvim',
+    'nvim-mini/mini.nvim',
     event = { 'BufReadPost', 'BufNewFile', 'CmdlineEnter' },
     config = function()
       require('mini.ai').setup { n_lines = 500 }
