90e2674b9e50217aa7da523282929d2a5732dfe6
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2025-09-10 21:04:38 -0400

    fix: gopls and lspconfig

--- .chezmoitemplates/nvim/lua/plugins/lsp.lua
+++ .chezmoitemplates/nvim/lua/plugins/lsp.lua
@@ -104,38 +104,42 @@ return {
         dockerls = {},
         eslint = {},
         gopls = {
-          gofumpt = true,
-          codelenses = {
-            gc_details = true,
-            generate = true,
-            regenerate_cgo = true,
-            run_govulncheck = true,
-            test = true,
-            tidy = true,
-            upgrade_dependency = true,
-            version = true,
+          settings = {
+            gopls = {
+              gofumpt = true,
+              codelenses = {
+                gc_details = true,
+                generate = true,
+                regenerate_cgo = true,
+                run_govulncheck = true,
+                test = true,
+                tidy = true,
+                upgrade_dependency = true,
+                version = true,
+              },
+              hints = {
+                assignVariableTypes = true,
+                compositeLiteralFields = true,
+                compositeLiteralTypes = true,
+                constantValues = true,
+                functionTypeParameters = true,
+                parameterNames = true,
+                rangeVariableTypes = true,
+              },
+              analyses = {
+                fieldalignment = true,
+                nilness = true,
+                unusedparams = true,
+                unusedwrite = true,
+                useany = true,
+              },
+              usePlaceholders = true,
+              completeUnimported = true,
+              staticcheck = true,
+              directoryFilters = { '-.git', '-.vscode', '-.idea', '-.vscode-test', '-node_modules' },
+              semanticTokens = true,
+            },
           },
-          hints = {
-            assignVariableTypes = true,
-            compositeLiteralFields = true,
-            compositeLiteralTypes = true,
-            constantValues = true,
-            functionTypeParameters = true,
-            parameterNames = true,
-            rangeVariableTypes = true,
-          },
-          analyses = {
-            fieldalignment = true,
-            nilness = true,
-            unusedparams = true,
-            unusedwrite = true,
-            useany = true,
-          },
-          usePlaceholders = true,
-          completeUnimported = true,
-          staticcheck = true,
-          directoryFilters = { '-.git', '-.vscode', '-.idea', '-.vscode-test', '-node_modules' },
-          semanticTokens = true,
         },
         harper_ls = {
           settings = {
@@ -351,6 +355,8 @@ return {
         vim.lsp.config(server, server_opts)
         if is_enabled(server_opts) == false then
           vim.lsp.enable(server, false)
+        else
+          vim.lsp.enable(server)
         end
       end
     end,
