21168f7a1bc52a15d04c0ddd51f12eb03a45139f
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2025-10-08 21:37:05 -0400

    updated sidekick.nvim api

--- .chezmoitemplates/nvim/lua/plugins/ai.lua
+++ .chezmoitemplates/nvim/lua/plugins/ai.lua
@@ -22,18 +22,33 @@ return {
         {
           '<c-.>',
           function()
-            require('sidekick.cli').focus()
-          end,
-          mode = { 'n', 'x', 'i', 't' },
-          desc = 'Sidekick Switch Focus',
-        },
-        {
-          '<leader>aa',
-          function()
             require('sidekick.cli').toggle { name = 'opencode', focus = true }
           end,
-          desc = 'Sidekick Toggle CLI',
-          mode = { 'n', 'v' },
+          mode = { 'n', 'x', 'i', 't' },
+          desc = 'Sidekick Toggle',
+        },
+        {
+          '<leader>at',
+          function()
+            require('sidekick.cli').send { msg = '{this}' }
+          end,
+          mode = { 'x', 'n' },
+          desc = 'Send This',
+        },
+        {
+          '<leader>af',
+          function()
+            require('sidekick.cli').send { msg = '{file}' }
+          end,
+          desc = 'Send File',
+        },
+        {
+          '<leader>av',
+          function()
+            require('sidekick.cli').send { msg = '{selection}' }
+          end,
+          mode = { 'x' },
+          desc = 'Send Visual Selection',
         },
         {
           '<leader>ap',
