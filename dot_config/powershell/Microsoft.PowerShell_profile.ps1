4f7a2ae79603022cfe156a631ab5cf50140b8261
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2026-01-16 13:15:19 -0500

    fix(wezterm): windows shell integration

--- dot_config/powershell/Microsoft.PowerShell_profile.ps1
+++ dot_config/powershell/Microsoft.PowerShell_profile.ps1
@@ -396,6 +396,18 @@ if ($PSVersionTable.PSVersion.Major -ge 6)
 {
     if (Get-Command starship -ErrorAction SilentlyContinue)
     {
+        $prompt = ""
+        function Invoke-Starship-PreCommand
+        {
+            $current_location = $executionContext.SessionState.Path.CurrentLocation
+            if ($current_location.Provider.Name -eq "FileSystem")
+            {
+                $ansi_escape = [char]27
+                $provider_path = $current_location.ProviderPath -replace "\\", "/"
+                $prompt = "$ansi_escape]7;file://${env:COMPUTERNAME}/${provider_path}$ansi_escape\"
+            }
+            $host.ui.Write($prompt)
+        }
         Invoke-Expression (&starship init powershell)
         Enable-TransientPrompt
     }
