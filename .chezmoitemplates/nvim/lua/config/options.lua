91c0d4b6e32397538c10a478a901a4cac58eafe4
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2026-01-18 12:17:38 -0500

    fix(nvim): foldexpr opts

--- .chezmoitemplates/nvim/lua/config/options.lua
+++ .chezmoitemplates/nvim/lua/config/options.lua
@@ -93,8 +93,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(args)
     local client = vim.lsp.get_client_by_id(args.data.client_id)
     if client and client:supports_method 'textDocument/foldingRange' then
-      local win = vim.api.nvim_get_current_win()
-      vim.wo[win][0].foldexpr = 'v:lua.vim.lsp.foldexpr()'
+      vim.wo.foldexpr = 'v:lua.vim.lsp.foldexpr()'
     end
   end,
 })
