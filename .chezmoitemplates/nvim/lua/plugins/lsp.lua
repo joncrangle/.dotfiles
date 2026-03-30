9a538c9d2394842960975ab9ae038ee26edc93ee
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2026-03-30 09:47:25 -0400

    chore(nvim): update deprecated codelens for 0.12

--- .chezmoitemplates/nvim/lua/plugins/lsp.lua
+++ .chezmoitemplates/nvim/lua/plugins/lsp.lua
@@ -78,11 +78,7 @@ return {
           end
 
           if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_codeLens, event.buf) then
-            vim.api.nvim_create_autocmd({ 'BufEnter', 'CursorHold', 'InsertLeave' }, {
-              buffer = event.buf,
-              callback = vim.lsp.codelens.refresh,
-            })
-            vim.lsp.codelens.refresh()
+            vim.lsp.codelens.enable(true, { bufnr = event.buf })
           end
 
           map('<leader>cr', vim.lsp.codelens.run, '[C]ode Lens [R]un')
