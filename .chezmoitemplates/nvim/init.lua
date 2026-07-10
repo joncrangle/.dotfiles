98578a7bb698269f249c8a2c81ed11b8a9bb0094
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2026-07-09 23:00:14 -0400

    perf(nvim): use vim.loader for faster startup

--- .chezmoitemplates/nvim/init.lua
+++ .chezmoitemplates/nvim/init.lua
@@ -20,6 +20,7 @@ NOTE:
 =====================================================================
 --]]
 
+vim.loader.enable()
 vim.g.mapleader = ' '
 vim.g.maplocalleader = '\\'
 vim.g.have_nerd_font = true
